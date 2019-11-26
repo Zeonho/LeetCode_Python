17e494f963ccf10faf42b4c2e83e2c7bbcf498ca
Jinhao <cjh6348768@hotmail.com>
2019-11-25 19:41:55 -0600

    hard

---
+++ 204_Count_Primes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countPrimes(self, n:int) -> int:
+        if n <= 2: 
+            return 0
+            
+        primes = [True] * n
+        primes[0] = primes[1] = False
+        for num in range(2, int(math.sqrt(n)) + 1):
+            if primes[num]:
+                primes[num**2:n:num] = [False] * len(primes[num**2:n:num])
+        
+        return sum(primes)
