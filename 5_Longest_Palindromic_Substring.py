141945ebc587b255f395f5539d0b3cbfe61b06f7
Jinhao <cjh6348768@hotmail.com>
2019-11-26 17:45:23 -0600

    Miss question

--- 5_Longest_Palindromic_Substring.py
+++ 5_Longest_Palindromic_Substring.py
@@ -5,4 +5,17 @@ You may assume that the maximum length of s is 1000.
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        
+        res = ""
+        for i in range(len(s)):        
+            odd  = self.palindromeAt(s, i, i)
+            even = self.palindromeAt(s, i, i+1)
+            res = max(res, odd, even, key=len)
+        return res
+
+    # Get the Lonest Palidrome, l, r are the middle indexes from inner 
+    # to oter
+    def helper(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1
+            r += 1
+        return s[l+1:r]
