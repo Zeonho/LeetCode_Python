5416bb9a1bfa3d6c1fb78652748ad8d6d5631840
Jinhao <cjh6348768@hotmail.com>
2019-11-28 17:02:08 -0600

    One more today

---
+++ 905_Sort_Array_By_Parity.py
@@ -0,0 +1,11 @@
+"""
+Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
+
+You may return any answer array that satisfies this condition.
+"""
+
+class Solution:
+    def sortArrayByParity(self, A: List[int]) -> List[int]:
+        A.sort(key = lambda x: x % 2)
+        return A
+        
