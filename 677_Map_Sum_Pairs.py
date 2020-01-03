a969e20c8dcb30561d633fcd57c6d493ec75efa0
Jinhao <cjh6348768@hotmail.com>
2020-01-03 12:52:14 -0600

    back

---
+++ 677_Map_Sum_Pairs.py
@@ -0,0 +1,42 @@
+"""
+Implement a MapSum class with insert, and sum methods.
+
+For the method insert, you'll be given a pair of (string, integer). The string represents the key and the integer represents the value. If the key already existed, then the original key-value pair will be overridden to the new one.
+
+For the method sum, you'll be given a string representing the prefix, and you need to return the sum of all the pairs' value whose key starts with the prefix.
+
+Example 1:
+Input: insert("apple", 3), Output: Null
+Input: sum("ap"), Output: 3
+Input: insert("app", 2), Output: Null
+Input: sum("ap"), Output: 5
+"""
+
+class TrieNode(object):
+    __slot__ = 'children', 'score'
+    def __init__(self):
+        self.children = {}
+        self.score = 0
+
+class MapSum(object):
+    def __init__(self):
+        self.map = {}
+        self.root = TrieNode()
+
+    def insert(self, key, val):
+        delta = val - self.map.get(key, 0)
+        self.map[key] = val
+        cur = self.root
+        cur.score += delta
+        for char in key:
+            cur = cur.children.setdefault(char, TrieNode())
+        cur.score += delta
+
+    def sum(self, prefix):
+        cur = self.root
+        for char in prefix:
+            if char not in cur.children:
+                return 0
+            cur = cur.children[char]
+
+        return cur.score
