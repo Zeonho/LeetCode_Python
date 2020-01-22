fa2044e26b779b94145358f8f477d40439f5d1f3
Jinhao <cjh6348768@hotmail.com>
2020-01-22 19:49:21 +0800

    modified

--- 867_Transpose_Matrix.py
+++ 867_Transpose_Matrix.py
@@ -9,10 +9,10 @@ switching the row and column indices of the matrix.
 
 class Solution:
     def transpose(self, A: List[List[int]]) -> List[List[int]]:
-        R, C = len(A), len(A[0])
-        ans = [[None] * R for _ in range(C)]
-        print ans
-        for r, row in enumerate(A):
-            for c, val in enumerate(row):
-                ans[c][r] = val
-            return ans
+        res = []
+        for i in range(len(A[0])):
+            temp  = []
+            for j in range(len(A)):
+                temp.append(A[j][i])
+                res.append(temp)
+        return res
