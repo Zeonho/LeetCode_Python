0a9c3cc8e9de7b1e62aa6e9f62f202b46592cb34
Ginhc <cjh6348768@hotmail.com>
2020-02-17 20:14:12 +0800

    one medium for today

---
+++ 116_Populating_Next_Right_Pointers_in_Each_Node.py
@@ -0,0 +1,73 @@
+"""
+116. Populating Next Right Pointers in Each Node
+Medium
+
+1517
+
+133
+
+Add to List
+
+Share
+You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+"""
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if root and root.left and root.right:
+            root.left.next = root.right
+            if root.next:
+                root.right.next = root.next.left
+            self.connect(root.left)
+            self.connect(roo.right)
+
+        return root
+
+    def connectBFS(self, root: 'Node') -> 'Node':
+        if not root:
+            return 
+        queue = [root]
+        while queue:
+            curr = queue.pop(0)
+            if curr.left and curr.right:
+                curr.left.next = curr.right
+                if curr.next:
+                    curr.right.next = curr.next.left
+                queue.append(curr.left)
+                queue.append(curr.right)
+
+        return root
+
+
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return
+        stack = [root]
+        while stack:
+            curr = stack.pop()
+            if curr.left and curr.right:
+                curr.left.next = curr.right
+                if curr.next:
+                    curr.right.next = curr.next.left
+                stack.append(curr.right)
+                stack.append(curr.left)
+
+        return root
