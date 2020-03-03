dc4ea8d79aea11e25fcef71b0eed0438ef77ef52
Ginhc <cjh6348768@hotmail.com>
2020-03-03 10:49:39 +0800

    amend content

--- 207_Course_Schedule.py
+++ 207_Course_Schedule.py
@@ -1,5 +1,5 @@
 """
-There are a total of n courses you have to take, labeled from 0 to n-1.
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses-1.
 Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
 Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
 Example 1:
@@ -20,4 +20,16 @@ You may assume that there are no duplicate edges in the input prerequisites.
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        
+        # BFS Topological Sorting
+        G = [[] for i in range(numCourses) ]
+        degree = [0] * numCourses
+        for j, i in prerequisites:
+            G[i].append(j)
+            degree[j] += 1
+        bfs = [i for i in range(numCourses) if degree[i] == 0]
+        for i in bfs:
+            for j in G[i]:
+                degree[j] -= 1
+                if degree[j] == 0:
+                    bfs.append(j)
+        return len(bfs) == numCourses
