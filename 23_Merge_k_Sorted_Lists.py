53555c0862e064c52bb5f8d1fe3e4fb56abd5f47
ginhc <cjh6348768@hotmail.com>
2020-03-10 20:48:54 -0700

    priority queue = sorted queue by heap

---
+++ 23_Merge_k_Sorted_Lists.py
@@ -0,0 +1,45 @@
+"""
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+Example:
+Input:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+Output: 1->1->2->3->4->4->5->6
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+from queue import PriorityQueue
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        class Wrapper():
+            def __init__(self, node):
+                self.node = node
+            def __lt__(self, other):
+                return self.node.val < other.node.val
+
+        head = point = ListNode(0)
+        q = PriorityQueue()
+        for l in lists:
+            if l:
+                q.put(Wrapper(l))
+        while not q.empty():
+            node = q.get().node
+            point.next = node
+            point = point.next
+            node = node.next
+            if node:
+                q.put(Wrapper(node))
+        return head.next
